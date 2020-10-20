b728b0e22ffb3fe09dac034d7e81a77bc41c6bcb
Kelvin Gorospe <kdgorospe@gmail.com>
2020-10-20 10:29:47 -0400

    fix clean_sci_names to match the names found in ncbi

--- 01_clean_lca_dat.R
+++ 01_clean_lca_dat.R
@@ -5,11 +5,11 @@ library(taxize)
 library(data.table)
 
 # Mac
-#datadir <- "/Volumes/jgephart/BFA Environment 2/Data"
-#outdir <- "/Volumes/jgephart/BFA Environment 2/Outputs"
+datadir <- "/Volumes/jgephart/BFA Environment 2/Data"
+outdir <- "/Volumes/jgephart/BFA Environment 2/Outputs"
 # Windows
-datadir <- "K:/BFA Environment 2/Data"
-outdir <- "K:BFA Environment 2/Outputs"
+# datadir <- "K:/BFA Environment 2/Data"
+# outdir <- "K:BFA Environment 2/Outputs"
 
 lca_dat <- read.csv(file.path(datadir, "LCA_compiled.csv"), fileEncoding="UTF-8-BOM") #fileEncoding needed when reading in file from windows computer (suppresses BOM hidden characters)
 
@@ -17,6 +17,8 @@ lca_dat <- read.csv(file.path(datadir, "LCA_compiled.csv"), fileEncoding="UTF-8-
 # Manually fill in blank scientific names
 # Clean species names: e.g., Change osteichthyes (technically includes all terapods) to actinopterygii (bony fishes)
 # Remove unnecessary columns
+# Manually change certain scientific names to match NCBI database: see function "test_match" below for explanation
+# Column clean_sci_name will be the "official" column used throughout code
 lca_fix_names <- lca_dat %>%
   mutate(Species.scientific.name = case_when(Species.common.name == "Freshwater prawn" ~ "Dendrobranchiata",
                                              Species.common.name == "Indo-Pacific swamp crab; Swimming crabs, etc. nei" ~ "Brachyura",
@@ -27,6 +29,8 @@ lca_fix_names <- lca_dat %>%
   mutate(clean_sci_name = case_when(str_detect(Species.scientific.name, "spp") ~ str_replace(Species.scientific.name, pattern = " spp\\.| spp", replacement = ""),
                                     Species.scientific.name == "Morone chrysops x M. saxatilis" ~ "Morone",
                                     Species.scientific.name == "Osteichthyes" ~ "Actinopterygii",
+                                    Species.scientific.name == "Litopenaeus vannamei" ~ "Penaeus vannamei",
+                                    Species.scientific.name == "Pangasius hypophthalmus" ~ "Pangasianodon hypophthalmus", 
                                     TRUE ~ Species.scientific.name)) %>%
   #filter(Species.scientific.name != "") %>% # blank scientific names manually filled in above
   mutate(FCR = case_when(str_detect(Species.scientific.name, "Thunnus") ~ FCR/5,
@@ -37,14 +41,15 @@ lca_fix_names <- lca_dat %>%
 ################################################################################################################
 # use package taxize to get higher classification levels for each species
 
-# LEFT OFF HERE:
-# FIX IT - need to get API key for ncbi eventually started getting the following message:
+# Get API key:
 # No ENTREZ API key provided
 # Get one via taxize::use_entrez()
 # See https://ncbiinsights.ncbi.nlm.nih.gov/2017/11/02/new-api-keys-for-the-e-utilities/
 #   Error: Too Many Requests (HTTP 429)
 
-# Use NCBI database? more resolution, but have to remove all the different "clade" ranks - can't merge on this name
+# Use NCBI database: more taxa resolution, but have to remove all the different "clade" ranks - can't merge on this name
+# also WORMS database wasn't able to match some of the taxa
+
 # classify_ncbi <- classification("Penaeus monodon", db = "ncbi")
 # classify_ncbi[[1]]
 # classify_worms <- classification("Penaeus monodon", db = "worms")
@@ -64,18 +69,34 @@ lca_fix_names <- lca_dat %>%
 
 # Get full species list
 species_list <- sort(unique(lca_fix_names$clean_sci_name))
-# TEST: species_list <- c("Penaeus monodon", "Oreochromis niloticus")
-# TEST just genus: species_list <- "Macrobrachium"
 
 # Use classification function to get higher ranks for all species
 # IMPORTANT: RUN THIS AS A SINGLE LINE, otherwise when function asks for further input Re: osteichthyes, it will accept next line of code as the input
 # NOTE: when prompted choose "bony fishes" for osteichthyes
-taxa_ranks <- lapply(species_list, classification, db = "worms")
+taxa_ranks <- lapply(species_list, classification, db = "ncbi")
 
 # Which taxa did not find a match in the classification database:
 classify_test <- data.frame(no_results = unlist(lapply(taxa_ranks, is.na))) %>%
   filter(no_results == TRUE)
 # classify_test is empty - all were matched in classification database
+
+# Which taxa were matched to a different name?
+# EXAMPLE: taxa_ranks[[17]]
+test_match <- function(taxa_ranks) {
+  # get the name of the last row
+  last_row <- nrow(taxa_ranks[[1]])
+  name_in_database <- taxa_ranks[[1]][last_row,]$name
+  queried_name <- names(taxa_ranks)
+  if (name_in_database != queried_name) {
+    print(paste("Manually change ", queried_name, " in LCA dataset to ", name_in_database, sep = ""))
+  }
+}
+
+lapply(taxa_ranks, test_match)
+
+# RESULT: Make these name changes at top of code
+# "Manually change Litopenaeus vannamei in LCA dataset to Penaeus vannamei"
+# "Manually change Pangasius hypophthalmus in LCA dataset to Pangasianodon hypophthalmus"
 
 # Function to reformat ranks into columns
 format_ranks <- function(taxa_ranks) {
