49bbe8975c1b2a676845377fb621846baed52d41
jagephart <gepharja@miamioh.edu>
2020-12-08 21:02:53 -0500

    Added initial code for weightings

--- process_data_for_analysis.R
+++ process_data_for_analysis.R
@@ -39,3 +39,48 @@ write.csv(lca_dat_clean_groups, file.path(datadir, "lca_clean_with_groups.csv"),
 #lca_dat_clean_groups %>% select(taxa_group_name, clean_sci_name) %>% unique() %>% arrange(taxa_group_name)
 #write.csv(data.frame(table(lca_dat_clean_groups$taxa_group_name)), file.path(outdir, "taxa_group_sample_size.csv"))
 #write.csv(lca_dat_clean_groups %>% select(taxa_group_name, clean_sci_name) %>% unique() %>% arrange(taxa_group_name), file.path(outdir, "taxa_group_composition.csv"))
+
+# Calculate production weightings for each taxa group
+prod_weightings <- fishstat_dat %>% 
+  filter(year > 2012) %>%
+  filter(unit == "t") %>%
+  filter(source_name_en %in% c("Aquaculture production (marine)", "Aquaculture production (brackishwater)", "Aquaculture production (freshwater)")) %>%
+  group_by(isscaap_group, species_scientific_name) %>%
+  summarise(total = sum(quantity, na.rm = TRUE))
+
+# FIX IT: the species merge isn't working. Need to do some species name cleaning 
+prod_weightings <-  prod_weightings %>% 
+  right_join(lca_dat_clean_groups, by = c("species_scientific_name" = "clean_sci_name", "isscaap_group")) 
+
+# Calculate the weighted averages for the feed components # FIX IT: Need to add in groupings (not finished yet)
+feed_fp <- read.csv(file.path(datadir, "Feed_impact_factors_20201203.csv"))
+
+faostat <- read.csv(file.path(datadir, "FAOSTAT_data_12-8-2020.csv"))
+faostat_summary <- faostat %>% 
+  filter(Unit == "tonnes") %>% 
+  group_by(Area, Item) %>%
+  summarise(Value = sum(Value, na.rm = TRUE)) %>%
+  filter(Value > 0) %>% 
+  mutate(Item_group = case_when(
+    (Item %in% c("Soybeans", "Cake, soybeans", )) ~ "Soy",
+    (Item %in% c("Flour, cassava", "Starch, cassava")) ~ "Cassava",
+    (Item %in% c()) ~ "Corn gluten meal",
+    (Item %in% c()) ~ "Maize",
+    (Item %in% c()) ~ "Peanut meal",
+    (Item %in% c()) ~ "Rapeseed meal",
+    (Item %in% c("Oil, rapeseed")) ~ "Rapeseed oil",
+    (Item %in% c()) ~ "Wheat",
+    (Item %in% c()) ~ "Rice bran",
+    (Item %in% c()) ~ "Sunflower meal",
+    (Item %in% c()) ~ "Wheat bran",
+    (Item %in% c()) ~ "Soy protein concentrate",
+    (Item %in% c()) ~ "Soy protein isolate",
+    (Item %in% c()) ~ "Soybean meal"
+  ))
+
+
+ "Chicken by-product meal"              "Chicken by-product oil"              
+[25] "Pork blood meal"                                                     
+[28]    
+
+
