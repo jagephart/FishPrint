00f70c2bb95f6f3403ab07df13684d732cb1a11c
Kelvin Gorospe <kdgorospe@gmail.com>
2021-05-11 12:32:21 -0400

    change filenames/paths

--- bayes_03_ghg_model.R
+++ bayes_03_ghg_model.R
@@ -13,7 +13,7 @@ rm(list = ls())
 library(tidyverse)
 library(rstan)
 library(data.table)
-library(countrycode) # part of clean.lca
+library(countrycode)
 library(bayesplot) # for mcmc_areas_ridges
 library(shinystan)
 library(brms)
@@ -30,8 +30,8 @@ outdir <- "/Volumes/jgephart/BFA Environment 2/Outputs"
 # STEP 1: LOAD AND FORMAT DATA
 
 # Load Data
-#lca_full_dat <- read.csv(file.path(datadir, "2021-05-05_lca-dat-imputed-vars_rep-sqrt-n-farms_live-weight.csv"), fileEncoding="UTF-8-BOM")
-lca_full_dat <- read.csv(file.path(datadir, "2021-05-05_lca-dat-imputed-vars_rep-sqrt-n-farms_edible-weight.csv"), fileEncoding="UTF-8-BOM")
+#lca_full_dat <- read.csv(file.path(outdir, "lca-dat-imputed-vars_rep-sqrt-n-farms_live-weight.csv"), fileEncoding="UTF-8-BOM")
+lca_full_dat <- read.csv(file.path(outdir, "lca-dat-imputed-vars_rep-sqrt-n-farms_edible-weight.csv"), fileEncoding="UTF-8-BOM")
 
 # Get farm-associated carbon footprints data
 # Add iso3c
@@ -112,7 +112,7 @@ set_allocation <- "Mass"
 # Choose CARBON for Impact.category
 impact <- "Global warming potential" # i.e., Carbon impact
 
-fp_dat <- read.csv(file.path(datadir, "weighted_feed_fp.csv")) %>%
+fp_dat <- read.csv(file.path(outdir, "weighted_feed_fp.csv")) %>%
   filter(Allocation == set_allocation) %>%
   mutate(ave_stressor_per_tonne = ave_stressor * 1000) # Multiply by 1000 to convert to kg CO2 per tonne
 
@@ -181,7 +181,7 @@ sci_to_tx <- lca_model_dat %>%
 # WEIGHTS:
 # Get sci-level weightings for generating taxa-level quantities:
 # IMPORTANT arrange by clean_sci_name so that the order matches data
-sci_prod_weights <- read.csv(file.path(datadir, "aqua_prod_weightings.csv")) %>%
+sci_prod_weights <- read.csv(file.path(outdir, "aqua_prod_weightings.csv")) %>%
   arrange(clean_sci_name)
 # Drop sci-names not found in the data
 sci_prod_weights <- sci_prod_weights %>%
